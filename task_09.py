a831b260271800a1853ad8ee4674c4fce0c7a634
Sergo Bushmakin <giant2788@yahoo.com>
2015-09-21 00:55:27 +0000

    completed warmup 3

--- task_09.py
+++ task_09.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+
+A paragraph that I added to make it multi-line.  This paragraph will not exceed
+80 columns
+
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
---
+++ task_12.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" task_12.py """
+
+import decimal
+
+import fractions
+
+INTVAL = decimal.Decimal('1')
+
+FLOATVAL = decimal.Decimal('0.1')
+
+DECVAL = fractions.Fraction('1/10')
+
+FRACVAL = fractions.Fraction(1, 10)
